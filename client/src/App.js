b9073ec5e5e24715e5fcf0cb27ca7f34130a2f62
nileshpratap <nileshpratap03@gmail.com>
2023-04-05 19:12:13 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,8 @@ import { themeSettings } from "./theme";
 import Navbar from "./pages/navbar";
 import { Login } from "@mui/icons-material";
 import PeopleListWidget from "./pages/widgets/PeopleListWidget";
-import dotenv from "dotenv";
 
 function App() {
-  dotenv.config();
-
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   console.log("hi");
